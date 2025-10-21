d7fe09558b2e3a79783e5dcfb61443a0f6251a46
Viniciusp123 <vinicius.paulichen@escola.pr.gov.br>
2025-10-21 10:16:02 -0300

    21/10

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave(){
     const texto = document.querySelector("entrada-da-texto").value;
-    const campoResultado = document.querySelector("#reultado-palavrachave");
+    const campoResultado = document.querySelector("#resultado-palavrachave");
     const palavras = texto.split(" ");
 
     campoResultado.textContent = palavras.join(", ")
