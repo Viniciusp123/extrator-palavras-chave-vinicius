db4706b7613f9c714d2a169e8e5ca7d5cfae72b4
Viniciusp123 <vinicius.paulichen@escola.pr.gov.br>
2025-10-21 09:23:56 -0300

    21

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave;")
 botaoMostraPalavras.addEventListener("click", mostraPalvrasChave);
 
 function mostraPalvrasChave(){
-    alert("fui Clicado!");
+    alert("Fui clicado!");
 }
